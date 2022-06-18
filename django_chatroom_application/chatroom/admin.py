bff5ed904561f22d1dc288896d3894939fdd98a4
Vicent Botella Ferragud <vicentbotella.93@gmail.com>
2022-06-18 02:54:04 +0200

    template style added, models 'Chat' and 'Message' created, makemigrations and migrate commands run, 'MessageAdmin' and 'ChatAdmin' classes created

--- django_chatroom_application/chatroom/admin.py
+++ django_chatroom_application/chatroom/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Chat, Message
+
+class ChatAdmin(admin.ModelAdmin):
+    fields = ('created_at',)
+    list_display = ('created_at',)
+
+class MessageAdmin(admin.ModelAdmin):    
+    fields = ('created_at', 'chat', 'author', 'text', 'receiver')    
+    list_display = ('created_at', 'chat', 'author', 'text', 'receiver')    
+    search_fields = ('text',)
 
 # Register your models here.
+
+admin.site.register(Chat, ChatAdmin)
+admin.site.register(Message, MessageAdmin)
